72b53acea5f1e96540cc15d6ae1b5b90091bf9a6
hyogeun <gyrms3785@naver.com>
2025-05-03 21:43:38 +0900

    05.03 insert test
    05.03 insert test

--- src/main/java/com/example/myapp/controller/HelloController.java
+++ src/main/java/com/example/myapp/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.example.myapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
 public class HelloController {
