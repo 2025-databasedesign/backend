5db519f2ac5311a0f386006ce1fe583460fa3b08
hyogeun <gyrms3785@naver.com>
2025-05-03 21:37:25 +0900

    05.03 insert test
    05.03 insert test

--- src/main/java/com/example/myapp/controller/HelloController.java
+++ src/main/java/com/example/myapp/controller/HelloController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String hello() {
         return "Hi from Local Spring Boot!";
+    }
+
+    @GetMapping("/insert-test")
+    public String insertTest() {
+        jdbcTemplate.execute("INSERT INTO employees (id, name, hire_date) VALUES (1000, 'GCP Connect', SYSDATE)");
+        return "Inserted";
     }
 }
